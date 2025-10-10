72af55877dca2017715ee18d28d5d2f9f07b7b98
Davs06 <santosdv40@gmail.com>
2025-10-10 15:32:14 -0300

    add page 404

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Não Encontrada - Daven & Iori</title>
+
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="stylesheet" href="assets/css/404.css">
+</head>
+
+<body>
+
+    <div class="container-404">
+        <h1>404</h1>
+        <h2>Página Não Encontrada</h2>
+        <p>Pedimos desculpas, mas o conteúdo que você procura não está aqui. Ele pode ter sido movido ou nunca existiu.
+        </p>
+        <a href="/" class="home-button">Voltar à Página Inicial</a>
+    </div>
+
+</body>
+
+</html>
